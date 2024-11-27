75fad635be4c08710b4bdbfbf41bda79ae93ea99
Sam <163355895+soySamCS@users.noreply.github.com>
2024-11-27 12:52:02 -0600

    Asignamos y eliminamos estilos con JS

--- 02-Estillos/script.js
+++ 02-Estillos/script.js
@@ -52,4 +52,73 @@ console.log(titulo.style);
 
 */
 
-console.log("Valores en linea. Color: " + titulo.style.color + " Fondo: " + titulo.style.backgroundColor);
+console.log("Valores en linea: Color: " + titulo.style.color + ". Fondo: " + titulo.style.backgroundColor + ".");
+
+/* 
+
+  Método de propiedad
+
+    Asignar el valor de la propiedad de estilo en el elemento seleccionado.
+
+      elemento.style.nombrePropiedad = "valor"
+
+    - Forma más directa y legible de asignar propiedades.
+    - Sólo admite asignar propiedades con formato camelCase.
+
+*/
+
+titulo.style.color = "greenyellow"
+titulo.style.backgroundColor = "darkblue"
+
+console.log("Valores asignados con JS: Color: " + titulo.style.color + ". Fondo: " + titulo.style.backgroundColor + ".");
+
+/* 
+
+  Método setProperty()
+
+    Asignamos una propiedad de estilo al elemento seleccionado
+
+      elemento.style.setProperty(nombrePropiedad, valor, important);
+
+    - Más flexible y poderoso.
+    - Las propiedades se escribem en formato kebab-case.
+    - Parámetro "important" es opcional.
+
+*/
+
+titulo.style.setProperty("color", "violet");
+titulo.style.setProperty("background-color", "lightblue", "important");
+
+console.log("Valores asignados con setProperty: Color: " + titulo.style.color + ". Fondo: " + titulo.style.backgroundColor + ".");
+
+/* 
+
+  Eliminar valores de propiedades
+
+    Usamos el método de propiedad y le definimos una cadena de texto vacía.
+
+      elemento.stlye.nombrePropiedad = ""
+
+    Esto elimina los valor previamente definidos en JS y los estilo en linea. (Solo aplica los de CSS)
+
+*/
+
+/* 
+titulo.style.color = "";
+titulo.style.backgroundColor = "";
+ */
+
+/* 
+
+  Método removeProperty
+
+    Esto elimina la propiedad de estilo.
+
+      elemento.style.removeProperty(nombrePropiedad);
+
+    - Las propiedades se pasan en fortmato kebab-case
+
+*/
+
+titulo.style.removeProperty("color");
+titulo.style.removeProperty("background-color");
