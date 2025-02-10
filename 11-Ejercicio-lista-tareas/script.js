3e01ecf24455f59793e37c1257e74febe8a3bfaf
Sam <163355895+soySamCS@users.noreply.github.com>
2025-02-10 17:02:18 -0600

    Funcionalidad de agregar tarea

--- 11-Ejercicio-lista-tareas/script.js
+++ 11-Ejercicio-lista-tareas/script.js
@@ -2,6 +2,7 @@
 
 const taskInput = document.getElementById("taskInput");
 const addButton = document.getElementById("addButton");
+const taskList = document.getElementById('taskList');
 
 //Add event listener
 
@@ -12,7 +13,34 @@ addButton.addEventListener("click", createTask)
 function createTask() {
     if(taskInput.value){
         console.log(taskInput.value);
-        taskInput.value = ""; // Regresamos el valor vacío al input
+
+        /* Creamos el contenedor de una tarea */
+        const taskItem = document.createElement("div");
+        taskItem.classList.add("task");
+
+        // Creamos el parrafo de la tarea
+        const taskText = document.createElement("p");
+        taskText.innerText = taskInput.value;
+
+        // Creamos el contenedor de los iconos
+        const taskIcons = document.createElement("div");
+        taskIcons.classList.add("task-icons");
+
+        // Crear el icono de check
+        const iconChek = document.createElement("i");
+        iconChek.classList.add("bi" , "bi-patch-check" , "icon-complete");
+
+        // Crear el icono de trash
+        const iconDelete = document.createElement("i");
+        iconDelete.classList.add("bi" , "bi-trash" , "icon-delete");
+
+        // Estructura de los elementos
+        taskIcons.append(iconChek,iconDelete);
+        taskItem.append(taskText, taskIcons);
+        taskList.append(taskItem);
+
+         // Regresamos el valor vacío al input
+        taskInput.value = "";
     } else {
         alert("Escribe una tarea, por favor 🤔")
     }
